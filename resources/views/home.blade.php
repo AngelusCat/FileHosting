884d6c83746280796511b0c300684d534f70cd65
Dr.Angelicus <Kittyhellokitty555@gmail.com>
2024-08-09 10:12:10 +0300

    ...

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
     <title>Файлообменник - uppu</title>
 </head>
 <body>
-    <form action="/uploadFile" method="post" enctype="multipart/form-data">
+    <form action="/uploadFile" method="post" enctype="multipart/form-data" id="form">
         @csrf
         <input type="file" name="file">
         <input type="text" name="description">
@@ -16,10 +16,26 @@
             Выберите статус файла.
         </p>
         <p>
-            <input type="radio" name="viewingStatus" value="public">Публичный<br>
-            <input type="radio" name="viewingStatus" value="private">Приватный
+            <input type="radio" name="viewingStatus" value="public" id="publicRadio">Публичный<br>
+            <input type="radio" name="viewingStatus" value="private" id="privateRadio">Приватный
+        <div style="display: none" id="visibilityPassword">
+            <p>Введите пароль: </p>
+            <input type="text" name="visibilityPassword" form="form">
+        </div>
         </p>
         <button>Отправить</button>
     </form>
+    <script>
+        let publicRadio = document.getElementById('publicRadio');
+        let privateRadio = document.getElementById('privateRadio');
+        let div = document.getElementById('visibilityPassword');
+
+        publicRadio.addEventListener('click', function () {
+            div.setAttribute('style', 'display: none');
+        });
+        privateRadio.addEventListener('click', function () {
+            div.setAttribute('style', 'display: run-in');
+        });
+    </script>
 </body>
 </html>
