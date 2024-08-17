e04353ded606c2c922ca38c37565b2f875486244
Dr.Angelicus <Kittyhellokitty555@gmail.com>
2024-08-17 13:34:10 +0300

    ...

--- app/Test/ReturnedFile.php
+++ app/Test/ReturnedFile.php
@@ -2,9 +2,21 @@
 
 namespace app\Test;
 
+use App\Enums\Disk;
 use app\Test\File;
 
 class ReturnedFile extends File
 {
+    public function unload(): string
+    {
+        $currentPath = $this->getStartOfPath($this->disk) . '/' . $this->getFolders($this->nameToSave) . '/' . $this->nameToSave;
+        if ($this->disk->name === 'public') {
+            return $currentPath;
+        }
+    }
 
+    private function getStartOfPath(Disk $disk): string
+    {
+        return ($disk->name === 'public') ? storage_path('app/public/images') : storage_path('app/files');
+    }
 }
