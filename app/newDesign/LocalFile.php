cecab8d1bbb8a5784c4f4481e0be6d5b74e2b193
Dr.Angelicus <Kittyhellokitty555@gmail.com>
2024-08-24 08:25:00 +0300

    ...

--- app/newDesign/LocalFile.php
+++ app/newDesign/LocalFile.php
@@ -16,7 +16,18 @@ class LocalFile extends File
 
     public function getDownloadPath(int $id): string
     {
-        return "/tmp/$id/$this->originalName";
+        return $this->prepareForDownload($id);
+    }
+
+    private function prepareForDownload(int $id): string
+    {
+        $savePath = storage_path("app/files/") . $this->getFolders($this->nameToSave) . $this->nameToSave;
+        $copyPath = storage_path("app/files/") . "tmp/$id/$this->nameToSave";
+        Storage::disk($this->disk->name)->makeDirectory("tmp/$id/");
+        copy($savePath, $copyPath);
+        $renamePath = storage_path("app/files/") . "tmp/$id/$this->originalName";
+        rename($copyPath, $renamePath);
+        return $renamePath;
     }
 
     public function save(string $content): void
