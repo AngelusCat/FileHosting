2bcb4d25b7e06bfb002be5e49bcf2426b62d8cf1
Dr.Angelicus <Kittyhellokitty555@gmail.com>
2024-09-05 08:42:33 +0300

    ...

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+import {createApp} from "vue";
+import formVue from "../vue-components/form.vue"
+
+createApp({
+    components: {
+        formVue
+    }
+}).mount('#app');
---
+++ resources/vue-components/form.vue
@@ -0,0 +1,77 @@
+<script setup>
+import {ref} from "vue";
+
+let fileFromDropArea = ref();
+let divStyle = ref("display: none");
+let requiredPassword = ref(null);
+let privatePassword = ref();
+function preventDefaults(e) {
+    e.preventDefault();
+    e.stopPropagation();
+}
+
+function handleDrop(e) {
+    e.preventDefault();
+    const files = e.dataTransfer.files;
+    if (files.length) {
+        fileFromDropArea.value = files;
+    }
+}
+
+function clickPublic()
+{
+    divStyle.value = "display: none";
+    requiredPassword.value = null;
+}
+
+function clickPrivate()
+{
+    divStyle.value = "display: run-in";
+    requiredPassword.value = true;
+}
+
+function generatePassword()
+{
+    fetch('/generatePassword').then(response => response.json()).then(password => privatePassword.value = password.password);
+}
+
+</script>
+
+<template>
+    <input type="file" name="file" v-bind:files="fileFromDropArea"><br><br>
+    <div id="drop-area" @dragover="preventDefaults" @dragenter="preventDefaults" @dragleave="preventDefaults" @drop="handleDrop">
+        Перетащите файл сюда
+    </div>
+    <br>
+    <input type="text" name="description"> Описание файла
+    <p>
+        Выберите статус файла.
+    </p>
+    <p>
+        <input type="radio" name="viewingStatus" value="public" @click="clickPublic">Публичный<br>
+        <input type="radio" name="viewingStatus" value="private" @click="clickPrivate">Приватный
+        <div v-bind:style="divStyle">
+            <p>Введите пароль: </p>
+            <input type="text" name="visibilityPassword" form="form" v-bind:required="requiredPassword" v-bind:value="privatePassword">&nbsp
+            <button type="button" @click="generatePassword">Сгенерировать пароль</button>
+        </div>
+    </p>
+    <button type="submit">Отправить</button>
+</template>
+
+<style scoped>
+body {
+    margin: 0;
+    padding: 20px;
+    font-family: sans-serif;
+}
+
+#drop-area {
+    width: 400px;
+    height: 200px;
+    text-align: center;
+    line-height: 200px;
+    border: 2px dashed #ccc;
+    cursor: pointer;
+}
+</style>
