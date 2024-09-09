2e3d2a29ffd54e9e078505cd6ae80cd292a337f8
Dr.Angelicus <Kittyhellokitty555@gmail.com>
2024-09-09 10:30:51 +0300

    ...

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,28 @@
 <?php
 
-use App\Entities\JWT;
 use App\Http\Controllers\FileHosting;
-use App\Services\JWTAuth;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
-Route::post('/uploadFile', [FileHosting::class, 'upload']);
+})->name('home');
+Route::post('/uploadFile', [FileHosting::class, 'upload'])->name('uploadFile');
 
-Route::get('/downloadFile/{file_id}', [FileHosting::class, 'download']);
+Route::get('/downloadFile/{file_id}', [FileHosting::class, 'download'])->name('downloadFile');
 
 Route::get('/generatePassword', function () {
     return response()->json(['password' => bin2hex(random_bytes(5))]);
-});
+})->name('generatePassword');
 
-Route::get('/testApi', function () {
+/*Route::get('/testApi', function () {
     $fileName = "welcome.blade.php";
     $contents = file_get_contents("C:/localhost/file/resources/views/welcome.blade.php");
     $status = "public";
     $response = Http::attach(
         'file', $contents, $fileName
     )->post("http://file/api/files");
-});
+});*/
 
 Route::get('show/{file_id}', [FileHosting::class, 'show'])->middleware(\App\Http\Middleware\UserCanViewTheFile::class);
 
