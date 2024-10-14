d814b9f48101e9665472dd59f9482a27c072fa6f
Dr.Angelicus <Kittyhellokitty555@gmail.com>
2024-10-14 10:39:00 +0300

    ...

--- tests/Unit/PermissionRTest.php
+++ tests/Unit/PermissionRTest.php
@@ -8,7 +8,6 @@ use App\Enums\ViewingStatus;
 use Illuminate\Http\Request;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\MockObject\Exception;
-//use PHPUnit\Framework\TestCase;
 use Tests\TestCase;
 
 class PermissionRTest extends TestCase
@@ -19,10 +18,38 @@ class PermissionRTest extends TestCase
     #[Test]
     public function viewingStatusIsPublic_permissionIsR(): void
     {
-        $request = self::createStub(Request::class);
         $file = self::createStub(File::class);
         $file->method("getViewingStatus")->willReturn(ViewingStatus::public);
+        $request = self::createStub(Request::class);
         $permissionR = new PermissionR($request, $file);
         self::assertSame("r", $permissionR->getPermission());
+    }
+
+    #[Test]
+    public function viewingStatusIsPrivate_isUserAuthenticatedReturnsTrue_permissionIsR(): void
+    {
+        $file = self::createConfiguredStub(File::class, [
+            'getViewingStatus' => ViewingStatus::private,
+            'getId' => 1
+        ]);
+        $jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmaWxlX2lkIjoxLCJwZXJtaXNzaW9ucyI6InJlYWRvbmx5In0=.576379177206ef4c013ad106b99ebc281b4b51c08d3831f503997778b6a4a983";
+        $request = self::createStub(Request::class);
+        $request->expects($this->any())->method("hasCookie")->willReturn(true);
+        $request->expects($this->any())->method("cookie")->willReturn($jwt);
+        $permissionR = new PermissionR($request, $file);
+        self::assertSame("r", $permissionR->getPermission());
+    }
+
+    #[Test]
+    public function viewingStatusIsPrivate_isUserAuthenticatedReturnsFalse_permissionIsDash(): void
+    {
+        $file = self::createConfiguredStub(File::class, [
+            'getViewingStatus' => ViewingStatus::private,
+            'getId' => 1
+        ]);
+        $request = self::createStub(Request::class);
+        $request->expects($this->any())->method("hasCookie")->willReturn(false);
+        $permissionR = new PermissionR($request, $file);
+        self::assertSame("-", $permissionR->getPermission());
     }
 }
